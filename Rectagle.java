eda7289d569e32ab7e2c6562dce6df4bfea15038
TasosTsoulk <tassos95@windowslive.com>
2015-06-04 22:04:15 +0300

    Update Rectagle.java

--- Rectagle.java
+++ Rectagle.java
@@ -1,4 +1,19 @@
 public class Rectangle extends Square {
-    public double width;
+    public double Width;
     System.out.println("int A" + A);
+    public double getEmv(){
+        return A*Width;
+    }
+    public double getPer(){
+        return 2.0*A+2.0*Width;
+    }
+    @Override public String toString(){
+        return "A rectangle with width =" +Width+ "which is a subclass of" +super.toString();
+    }
+     public void setWidth(double Width){
+        this.Width=Width;
+    }
+    public double getWidth(){
+        return Width;
+    }
 }
